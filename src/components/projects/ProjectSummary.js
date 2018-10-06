817342dee4c547eac8a04504b59e9e15dd49c4d0
HR <hrui1990@gmail.com>
2018-10-06 00:48:48 -0500

    display dummy data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted by Rui Huang</p>
         <p className="grey-text">9/20/2018</p>
       </div>

212b61f419b8528fdec3788d5256ad1d399b0b26
HR <hrui1990@gmail.com>
2018-10-07 02:14:50 -0500

    create function backend done

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,6 +2,21 @@ export const createProject = project => {
   // normally return an object, but thunk can return a function
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to db
-    dispatch({ type: "CREATE_PROJECT", project });
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Rui",
+        authorLastName: "Huang",
+        authorID: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERROR", err });
+      });
   };
 };

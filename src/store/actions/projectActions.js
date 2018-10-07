55a0e8c51fde9266677be83820514a50c1507179
HR <hrui1990@gmail.com>
2018-10-07 02:00:01 -0500

    connect to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,6 @@
 export const createProject = project => {
   // normally return an object, but thunk can return a function
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to db
     dispatch({ type: "CREATE_PROJECT", project });
   };
